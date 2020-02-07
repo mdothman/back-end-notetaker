a76c2dfc8b6cc95001a0a70d19e1189a64b4f0cf
Rooster <othmanmuhidean@gmail.com>
2020-02-06 17:41:58 -0800

    Successfully pushed notes into db!

--- Develop/routes/api_routes.js
+++ Develop/routes/api_routes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
     app.get("/api/notes",
         function(req, res) {
             res.json(notes)
-
+            console.log(notes)
         });
     app.post("/api/notes",
         function(req, res) {
@@ -17,33 +17,15 @@ module.exports = function(app) {
 
 
             res.json(notes)
+            notes.push(data)
+
         });
     app.delete("/api/notes:id",
         function(req, res) {
             res.json(notes)
         });
 
-    function handleRequest(req, res) {
-        var path = req.url;
 
-        if (path) {
-
-            return renderNotesPage(req, res);
-
-        };
-
-    };
-
-    function renderIndexPage(req, res) {
-        fs.readFile(__dirname + "notes.html", function(err, data) {
-            if (err) {
-                res.writeHead(500, { "Content-Type": "text/html" });
-                res.end("<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>")
-            } else {
-
-            }
-        })
-    }
 
 
 
--- Develop/routes/html_routes.js
+++ Develop/routes/html_routes.js
@@ -6,10 +6,22 @@ module.exports = function(app) {
 
     app.get("/notes", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
+
+        function renderSavedNotes(notes) {
+            fs.readFile("./db/db.json", function(err, data) {
+                if (err) {
+                    throw err
+                } else {
+                    res.renderNoteList(data)
+                }
+
+            })
+        }
     });
     app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
+
 
 
 
